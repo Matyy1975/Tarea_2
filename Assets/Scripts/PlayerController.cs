3c437af18ac8058d72d62a166452dc963bb3fc79
Ssssoar <jano20@vtr.net>
2023-05-08 19:47:26 -0400

    Movimiento + Insta-drop
    Agregué funcionalidad básica para el insta-drop del jugador (por ahora un cuadrado).
    Se mantiene congelado por un tiempo editable.
    
    TODO: reacciónes al insta-drop de objetos (enemigos) adyacentes.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,49 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour{
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     private bool isJumping = false;
     private Rigidbody2D rb;
-    public GameObject childObject; // Objeto hijo cuyo SpriteRenderer se modificará
+    private SpriteRenderer sr;
+    public GameObject childObject; // Objeto hijo cuyo SpriteRenderer se modificara
+    
+    //Teclas
+    public KeyCode instantDropKey = KeyCode.S;
+    public float instaDropFreezeTime = 1f;
+    private float freezeTime = 0f;
 
-    void Start()
-    {
+    void Start(){
         rb = GetComponent<Rigidbody2D>();
+        sr = childObject.GetComponent<SpriteRenderer>();
     }
 
-    void Update()
-    {
-        // Movimiento horizontal
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(horizontalInput, 0, 0) * Time.deltaTime * moveSpeed;
-
-        // Flip sprite renderer del objeto hijo
-        SpriteRenderer sr = childObject.GetComponent<SpriteRenderer>();
-        if (horizontalInput < 0)
-        {
-            sr.flipX = true;
-        }
-        else if (horizontalInput > 0)
-        {
-            sr.flipX = false;
-        }
-
-        // Salto
-        if (Input.GetButtonDown("Jump") && !isJumping)
-        {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            isJumping = true;
+    void Update(){
+        if (freezeTime > 0){
+            //Freeze movement if we've just insta-dropped
+            freezeTime -= Time.deltaTime;
+        }else{
+            // Movimiento horizontal
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+            // Flip sprite renderer del objeto hijo
+            if (horizontalInput < 0){
+                sr.flipX = true;
+            }
+            else if (horizontalInput > 0){
+                sr.flipX = false;
+            }
+            // Salto
+            if (Input.GetButtonDown("Jump") && !isJumping){
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                isJumping = true;
+            }
+            // Instant drop
+            if (Input.GetKeyDown(instantDropKey)){
+                InstantDrop();
+            }
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Ground")){
             isJumping = false;
+        }
+    }
+
+    void InstantDrop(){
+        // Cast a ray down to check for the ground below the player
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, LayerMask.GetMask("Ground"));
+        // Check if the ray hit anything
+        if (hit.collider != null){
+            // Move the player to the closest ground position
+            transform.position = new Vector3(transform.position.x, hit.point.y + GetComponent<BoxCollider2D>().size.y/2f, transform.position.z);
+            // Kill all velocity and forces acting on Player
+            rb.velocity = Vector2.zero;
+            rb.Sleep();
+            freezeTime = instaDropFreezeTime;
         }
     }
 }
