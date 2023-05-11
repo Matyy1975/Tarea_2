6cd6d5b37b3e5c755001f4909aa862540f0c5319
Ssssoar <jano20@vtr.net>
2023-05-11 09:49:55 -0400

    Arreglé el bug que te permite saltar en el aire.
    El bug era por solo cambiar la variable "airborne" al ejecutar un salto, y no al caer de alguna plataforma.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -58,11 +58,17 @@ public class PlayerController : MonoBehaviour{
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision){
-        if (collision.gameObject.CompareTag("Ground")){
+void OnCollisionEnter2D(Collision2D collision){
+    //Ground here just means game Terrain, as in, not enemies or other elements. could be walls
+    if (collision.gameObject.CompareTag("Ground")){
+        if (Mathf.Abs(collision.contacts[0].normal.y) < 0.1f){
+            // If the collision is with a wall, set the player as airborne
+            airborne = true;
+        }else{
             airborne = false;
         }
     }
+}
     void OnCollisionExit2D(Collision2D collision){
         if (collision.gameObject.CompareTag("Ground")){
             airborne = true;
