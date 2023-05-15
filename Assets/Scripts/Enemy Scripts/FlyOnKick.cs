3204eef4b7c9d90fdb0362bc30c6d626ea9524af
Ssssoar <jano20@vtr.net>
2023-05-15 15:38:49 -0400

    Patadas violentas!
    La patada ahora es más violenta, el enemigo deja de ser afectado por la gravedad hasta colisionar con una de las paredes, para que el apuntar sea mas satisfactorio

--- Assets/Scripts/Enemy Scripts/FlyOnKick.cs
+++ Assets/Scripts/Enemy Scripts/FlyOnKick.cs
@@ -6,23 +6,37 @@ public class FlyOnKick : MonoBehaviour{
     public GameObject aimerObject;
     public float kickForce = 3f;
     private Rigidbody2D rb;
+    private float backupGrav;
     // Start is called before the first frame update
     void Start(){
         //get your own rigidbody into rb
         rb = GetComponent<Rigidbody2D>();
+        backupGrav = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update(){
         
     }
+    
+    //used to restore the object's gravity when it collides with anything
+    void OnCollisionEnter2D (){
+        if (rb.gravityScale == 0){
+            rb.gravityScale = backupGrav;
+        }
+    }
+    
     //Called when DetectKick decides that it has been kicked by the player
     public void Execute(){
         //Get Zrotation from the aimer child gameobject
         Vector2 directionVect = aimerObject.transform.up.normalized;
         Vector2 forceVect = directionVect * kickForce;
+        //nullify all forces, then apply a force in that direction.
         rb.velocity = Vector3.zero;
         rb.AddForce(forceVect, ForceMode2D.Impulse);
         aimerObject.SetActive(false);
+        //disable gravity so it flies in a straight line (and make sure to store a backup)
+        backupGrav = rb.gravityScale;
+        rb.gravityScale = 0;
     }
 }
