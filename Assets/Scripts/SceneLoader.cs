1a1a5ab3fed02396879b3bf1e36cc3dff31ea853
Ssssoar <jano20@vtr.net>
2023-06-01 08:29:08 -0400

    Glep
    Jojó

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,11 +5,33 @@ public class SceneLoader : MonoBehaviour
 {
     // Nombre de la escena que quieres cargar
     public string sceneName;
+    public float delay = 1f;
+    private bool transitioning = false;
+    private float timePassed = 0f;
+    private Animator fadeAnimator;
+
+    public void Start()
+    {
+        fadeAnimator = GameObject.Find("Fade").GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        if (transitioning)
+        {
+            timePassed += Time.deltaTime;
+            if (timePassed >= delay)
+            {
+                Time.timeScale = 1f;  // Reanudar la escala de tiempo normal
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
 
     public void LoadScene()
     {
-        Time.timeScale = 1f;  // Reanudar la escala de tiempo normal
-        SceneManager.LoadScene(sceneName);
+        fadeAnimator.SetBool("Fade", true);
+        transitioning = true;
     }
 
     public void QuitGame()
